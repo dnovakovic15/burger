721bbda4ea72f7d269fefbc288d8ea02b1eb89c2
dnovakovic15 <dnovakovic21@yahoo.com>
2017-12-19 18:04:08 -0600

    Heroku JawsDB addition

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 let mysql = require('mysql');
 
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Myfavri0te21",
-  database: "burgers_db"
-});
+let con;
+
+if(process.env.JAWSDB_URL){
+  con = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Myfavri0te21",
+    database: "burgers_db"
+  })
+}
 
 con.connect(function(err) {
   if (err) throw err;
